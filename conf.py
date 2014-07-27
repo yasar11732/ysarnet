a433ebd5aa1e15fb17a20d4b8f83f411e2edf314
Yaşar <yasar11732@gmail.com>
2014-07-27 05:09:45 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -295,18 +295,7 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-
-from os.path import dirname, join
-
-cred = open("ftp_credentials.txt").readlines()
-
-DEPLOY_COMMANDS = ["ftpsync.py ftp://{uname}:{psswd}@{ftp}/{remotedir} {localdir} --upload".format(
-        uname=cred[1].strip(),
-        psswd=cred[2].strip(),
-        ftp=cred[0].strip(),
-        remotedir='httpdocs',
-        localdir=join(dirname(__file__),"output")
-    )]
+# DEPLOY_COMMANDS = []
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
